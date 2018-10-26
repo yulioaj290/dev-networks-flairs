6ec0017739964495192886785c01e4752590a9b5
yulioaj290 <yulioaj290@github.com>
2018-10-26 00:05:21 -0400

    Updated and concatenated dev networks flais builder into one file. Created file of themes.

---
+++ dev-networks-flairs.js
@@ -0,0 +1,227 @@
+/**
+ * Created by yulio on 25/10/18.
+ */
+(function ($, themes, c) {
+
+    var API_GITHUB = "https://api.github.com/";
+    var API_STACK_OVERFLOW = "https://api.stackexchange.com/";
+    var profile = {};
+
+    githubTmpData = {
+        "login": "yulioaj290",
+        "id": 15785234,
+        "node_id": "MDQ6VXNlcjE1Nzg1MjM0",
+        "avatar_url": "https://avatars3.githubusercontent.com/u/15785234?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/yulioaj290",
+        "html_url": "https://github.com/yulioaj290",
+        "followers_url": "https://api.github.com/users/yulioaj290/followers",
+        "following_url": "https://api.github.com/users/yulioaj290/following{/other_user}",
+        "gists_url": "https://api.github.com/users/yulioaj290/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/yulioaj290/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/yulioaj290/subscriptions",
+        "organizations_url": "https://api.github.com/users/yulioaj290/orgs",
+        "repos_url": "https://api.github.com/users/yulioaj290/repos",
+        "events_url": "https://api.github.com/users/yulioaj290/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/yulioaj290/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": "Yulio Aleman Jimenez",
+        "company": "University of Informatics Sciences",
+        "blog": "https://yulioaj290.github.io",
+        "location": "Havana, Cuba",
+        "email": null,
+        "hireable": true,
+        "bio": "Computer science engineer and full-stack web developer specialized in PHP and JavaScript, looking for challenge opportunities to grow career.",
+        "public_repos": 16,
+        "public_gists": 1,
+        "followers": 0,
+        "following": 2,
+        "created_at": "2015-11-10T14:57:26Z",
+        "updated_at": "2018-10-22T16:59:23Z"
+    };
+
+    stackofTmpData = {
+        "items": [
+            {
+                "badge_counts": {
+                    "bronze": 19,
+                    "silver": 5,
+                    "gold": 0
+                },
+                "view_count": 133,
+                "answer_count": 44,
+                "question_count": 7,
+                "account_id": 8105646,
+                "is_employee": false,
+                "last_modified_date": 1540220638,
+                "last_access_date": 1540314638,
+                "reputation": 676,
+                "creation_date": 1458783157,
+                "user_type": "registered",
+                "user_id": 6107280,
+                "accept_rate": 100,
+                "location": "Havana, Cuba",
+                "website_url": "http://yulioaj290.github.io",
+                "link": "https://stackoverflow.com/users/6107280/yulio-aleman-jimenez",
+                "profile_image": "https://lh4.googleusercontent.com/-M76AbhcTzT8/AAAAAAAAAAI/AAAAAAAAAH4/fXMrjoU702c/photo.jpg?sz=128",
+                "display_name": "Yulio Aleman Jimenez"
+            }
+        ],
+        "has_more": false,
+        "quota_max": 300,
+        "quota_remaining": 295,
+        "type": "user"
+    };
+
+    function buildFlairs() {
+        flair = $('[data-flair]');
+
+        if (flair.length) {
+            var network = "";
+            flair.each(function () {
+                network = $(this).data("flair");
+
+                var dataTheme = $(this).data("theme");
+
+                if (dataTheme !== undefined && dataTheme !== false && themes !== undefined) {
+                    $(this).html(themes[dataTheme]);
+                }
+                var userid = getInput($(this));
+
+                // if (theme !== undefined && theme !== false && theme !== '') {
+                //         c(network);
+                //     $(this).load(theme, function () {
+                //         afterLoadTheme($(this), network, userid);
+                //     });
+                // } else {
+                afterSetTheme($(this), network, userid);
+                // }
+
+                profile = {};
+
+            });
+        }
+    }
+
+    function afterSetTheme(flairItem, network, userid) {
+
+        //--------------------TMP
+        if (network === "github") {
+            getProfile(network, githubTmpData);
+        } else if (network === "stackoverflow") {
+            getProfile(network, stackofTmpData);
+        }
+        setProperties(flairItem);
+        //--------------------TMP
+
+
+        // if (!userid) {
+        //     c("DEV NETWORKS FLAIRS [ERROR]: You must to provide a user id of " + network + ".");
+        // } else {
+        //     getUser(network, userid).then(function (data) {
+        //         getProfile(network, data);
+        //         setProperties(flairItem);
+        //     });
+        // }
+    }
+
+    function getInput(node) {
+        // c(node.data('user'));
+        return node.data('user');
+    }
+
+    function getUser(network, userid) {
+        var url;
+
+        if (network === "github") {
+            url = API_GITHUB + 'users/' + userid;
+        } else if (network === "stackoverflow") {
+            url = API_STACK_OVERFLOW + '2.2/users/' + userid + "?order=desc&sort=reputation&site=stackoverflow&filter=!Lot_8zr6DBKY4h6OoYkbF*";
+        }
+
+        return $.get(url)
+            .done(function () {
+                c("DEV NETWORKS FLAIRS [INFO]: The " + network + " user id data was retrieved successfully.");
+            })
+            .fail(function () {
+                c("DEV NETWORKS FLAIRS [ERROR]: The " + network + " user id is not valid or was not found.");
+            });
+    }
+
+    function getProfile(network, userData) {
+        if (network === "github") {
+            getProfileGithub(userData);
+        } else if (network === "stackoverflow") {
+            userData = userData['items'][0];
+            getProfileStackof(userData);
+        }
+    }
+
+    function getProfileGithub(userData) {
+        profile.username = userData['login'];
+        profile.fullname = userData['name'];
+        profile.avatar = userData['avatar_url'];
+        profile.link = userData['html_url'];
+        profile.company = userData['company'];
+        profile.location = userData['location'];
+        profile.website = userData['blog'];
+        profile.repos = truncateNum(userData['public_repos']);
+        profile.gists = truncateNum(userData['public_gists']);
+        profile.followers = truncateNum(userData['followers']);
+        profile.hireable = truncateNum(userData['hireable']);
+        profile.bio = truncateNum(userData['bio']);
+    }
+
+    function getProfileStackof(userData) {
+        profile.userid = userData['user_id'];
+        profile.fullname = userData['display_name'];
+        profile.avatar = userData['profile_image'];
+        profile.link = userData['link'];
+        profile.location = userData['location'];
+        profile.website = userData['website_url'];
+        profile.reputation = truncateNum(userData['reputation']);
+        profile.answers = truncateNum(userData['answer_count']);
+        profile.questions = truncateNum(userData['question_count']);
+        profile['badge-bronze'] = truncateNum(userData['badge_counts']['bronze']);
+        profile['badge-silver'] = truncateNum(userData['badge_counts']['silver']);
+        profile['badge-gold'] = truncateNum(userData['badge_counts']['gold']);
+    }
+
+    function setProperties(flairItem) {
+        for (var property in profile) {
+            if (profile.hasOwnProperty(property)) {
+                flairItem.find('[data-property="' + property + '"]').each(function () {
+                    $(this).text(profile[property]);
+                });
+                setLink(flairItem, property, profile[property]);
+            }
+        }
+    }
+
+    function truncateNum(number) {
+        var num = parseInt(number, 10);
+        if (num < 1000) return num;
+        return (num / 1000).toFixed(1) + 'k';
+    }
+
+    function setLink(flairItem, property, value) {
+        flairItem.find('[data-property="' + property + '"]').each(function () {
+            var link = $(this).data("link");
+
+            if (link === "link") {
+                $(this).attr("href", value);
+                // $(this).text("");
+                $(this).text("%");
+            } else if (link === "text") {
+                $(this).attr("href", value);
+                $(this).text(value);
+            }
+        });
+    }
+
+    window.onload = function () {
+        buildFlairs();
+    }
+
+})(window.jQuery, window.flairThemes, console.log);
